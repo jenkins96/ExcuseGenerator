cefdb54e8df5983a5feed9c8ae6c7e1bc6a19369
jenkins96 <adrianjnkns@gmail.com>
2021-02-06 15:55:15 +0000

    agregando mas personajes y acciones

--- function.js
+++ function.js
@@ -1,10 +1,10 @@
-let who = ['Donald Trump','El Porcionzón','Ignacio Santos','Arnold Schwarzenegger'];
-let action = ['burned','peed','crushed','kissed', 'took the axe and destroyed', 'smelled'];
-let what = ['my 4Geeks homework', 'my laptop', 'my crotch', 'our family dinner', 'my internet'];
-let when = ['before COVID-19','right when my crush was passing by','when I was taking a dump','during my karaoke session','while I was fishing'];
+let who = ['Donald Trump','El Porcionzón','Ignacio Santos','Arnold Schwarzenegger', 'Bryan Ruiz', 'Beyonce', "The gardener"];
+let action = ['burned','peed','crushed','kissed', 'took the axe and destroyed', 'smelled', 'ate', 'smashed', 'sat on', 'farted'];
+let what = ['my 4Geeks homework', 'my laptop', 'my crotch', 'our family dinner', 'my internet', 'my new shoes', 'my great ass'];
+let when = ['before COVID-19','right when my crush was passing by','when I was taking a dump','during my karaoke session','while I was fishing', 'during recess', 'after work'];
 
 function randomValue(arr, x){
-    let vindex = Math.floor(Math.random() * (x - 1));
+    let vindex = Math.floor(Math.random() * (x));
     return arr[vindex];
 } 
 let htmlwho = randomValue(who, who.length);
